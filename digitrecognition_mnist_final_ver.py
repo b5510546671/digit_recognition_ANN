5f55370c26c1710d2cb2d8f8b8ae1a02a3b15f6c
Knot Supavit <supavit.knot@gmail.com>
2017-05-27 14:11:07 +0700

    fixed bug

--- digitrecognition_mnist_final_ver.py
+++ digitrecognition_mnist_final_ver.py
@@ -13,101 +13,111 @@ from scipy import ndimage, misc
 import matplotlib.pyplot as plt
 K.set_image_dim_ordering('th')
 
-
-def baseline_model():
-    # create model
+def cnn_model():
     model = Sequential()
-    model.add(Conv2D(32, (5,5), input_shape=(1,28,28), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Conv2D(30, (5, 5), input_shape=(1, 28, 28), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(15, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.2))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
+    model.add(Dense(50, activation='relu'))
     model.add(Dense(num_classes, activation='softmax'))
-
-    # compile model
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
 # load training and testing datasets
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+(image_data_train, label_train), (image_data_test, label_test) = mnist.load_data()
 
-print ("1: {}".format(type(X_train)))
-print ("11: {}".format(X_train.shape))
-print ("11: {}".format(X_test.shape))
-print ("11: {}".format(y_train.shape))
-print ("11: {}".format(y_test.shape))
+print ("1: {}".format(type(image_data_train)))
+print ("11: {}".format(image_data_train.shape))
+print ("11: {}".format(image_data_test.shape))
+print ("11: {}".format(label_train.shape))
+print ("11: {}".format(label_test.shape))
 
 # reshape train/test dataset to be [samples][pixels][width][height]
-X_train = X_train.reshape(X_train.shape[0],1,28,28).astype('float32')
-X_test = X_test.reshape(X_test.shape[0],1,28,28).astype('float32')
+image_data_train = image_data_train.reshape(image_data_train.shape[0],1,28,28).astype('float32')
+image_data_test = image_data_test.reshape(image_data_test.shape[0],1,28,28).astype('float32')
 
-print ("2: {}".format(type(X_train)))
-print ("22: {}".format(X_train.shape))
-print ("22: {}".format(X_test.shape))
-print ("22: {}".format(y_train.shape))
-print ("22: {}".format(y_test.shape))
-# print ("22: {}".format(X_train))
-# print ("22: {}".format(X_test))
+print ("2: {}".format(type(image_data_train)))
+print ("22: {}".format(image_data_train.shape))
+print ("22: {}".format(image_data_test.shape))
+print ("22: {}".format(label_train.shape))
+print ("22: {}".format(label_test.shape))
 
 # normalize inputs from 0-255 to 0-1
-X_train = X_train / 255
-X_test = X_test / 255
+image_data_train = image_data_train / 255
+image_data_test = image_data_test / 255
 
-print ("3: {}".format(type(X_train)))
-print ("33: {}".format(X_train.shape))
-print ("33: {}".format(X_test.shape))
-# print ("33: {}".format(X_train))
-# print ("33: {}".format(X_test))
+print ("3: {}".format(type(image_data_train)))
+print ("33: {}".format(image_data_train.shape))
+print ("33: {}".format(image_data_test.shape))
 
 # one hot encode outputs
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
-num_classes = y_test.shape[1]
+#transform vector of class integers into a binary matrix
+label_train = np_utils.to_categorical(label_train)
+label_test = np_utils.to_categorical(label_test)
+num_classes = label_test.shape[1]
 
-# build the model
-model = baseline_model()
+
+model = cnn_model()
 # fit the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
+model.fit(image_data_train, label_train, validation_data=(image_data_test, label_test), epochs=10, batch_size=200, verbose=2)
 # final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Baseline Error: %.2f%%" % (100-scores[1]*100))
+scores = model.evaluate(image_data_test, label_test, verbose=0)
+print("Error: %.2f%%" % (100-scores[1]*100))
 
 while True:
 
-    photo_num = input("Enter image number: ")
+    input_file_number = input("Enter image number or 'exit' to exit: ")
 
-    if str(photo_num) == "exit":
+    if str(input_file_number) == "exit":
         break
 
-    # try with our own digit image for prediction
-    filepath = "C:\\Users\\knotsupavit\\Desktop\\ann\\neural2d\\images\\mnist\\train-data\\" + str(photo_num) + ".bmp"
+    elif input_file_number.isdigit():
+        # try with our own digit image for prediction
+        filepath = "C:\\Users\\knotsupavit\\Desktop\\ann\\neural2d\\images\\mnist\\validate-data\\" + str(input_file_number) + ".bmp"
 
-    photo_predict = ndimage.imread(filepath, mode='L')
+        photo_predict = ndimage.imread(filepath, mode='L')
 
-    print ("4: {}".format(type(photo_predict)))
+        print ("4: {}".format(type(photo_predict)))
 
-    print ("5: {}".format(photo_predict.shape))
+        print ("5: {}".format(photo_predict.shape))
 
-    # photo_predict = photo_predict.reshape(28,28)
-    # photo_predict = photo_predict.reshape(photo_predict.shape[0],1,28,28).astype('float32')
+        # show image
+        plt.imshow(photo_predict)
+        plt.show()
 
-    plt.imshow(photo_predict)
-    plt.show()
+        print ("6: {}".format(type(photo_predict)))
 
-    # f = np.load(filepath)
-    print ("6: {}".format(type(photo_predict)))
+        print ("7: {}".format(photo_predict.shape))
 
-    print ("7: {}".format(photo_predict.shape))
+        photo_predict_preprocessed = np.array([[photo_predict]])
+        photo_predict_preprocessed = photo_predict_preprocessed / 255
+        print ("7.5: {}".format(photo_predict_preprocessed.shape))
 
-    photo_predict1 = np.array([[photo_predict]])
-    photo_predict1 = photo_predict1 / 255
-    print ("7.5: {}".format(photo_predict1.shape))
 
-    # photo_predict1 = np_utils.to_categorical(photo_predict1)
+        print ("8: {}".format(type(photo_predict_preprocessed)))
 
-    print ("8: {}".format(type(photo_predict1)))
+        print ("9: {}".format(photo_predict_preprocessed.shape))
 
-    print ("9: {}".format(photo_predict1.shape))
+        prediction = model.predict(photo_predict_preprocessed).tolist()
+        print(prediction[0])
 
-    prediction = model.predict(photo_predict1)
-    print(prediction)
+        print(type(prediction[0]))
+
+        count = 0
+        max_num = -1000000000
+        predict_num = 0
+        for x in prediction[0]:
+            if x > max_num:
+                max_num = x
+                predict_num = count
+            count += 1
+
+        print("Predict digit: {}".format(predict_num))
+
+    else:
+        print("Invalid input!")
+
